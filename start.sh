7777372ede556c60f101fb340e2b86c3b7d1fab3
jihong.zjh <jihong.zjh@alibaba-inc.com>
2015-12-11 11:46:50 +0800

    update

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 source ~/.bash_profile
 hexo3 generate && hexo2 o
-cp -r public/* myblog
+#cp -r public/* myblog
+rsync -arzv public/* myblog
 cd myblog
 git add .
 git commit -a -m "add new"
