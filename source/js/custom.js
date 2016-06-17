46e5b13a3c326ea7b66524108007b7f5447f2f19
jihong.zjh <jihong.zjh@alibaba-inc.com>
2016-06-17 17:22:46 +0800

    增加打赏功能

---
+++ source/js/custom.js
@@ -0,0 +1,11 @@
+/*
+ * Des：用户自定义js脚本
+ * Author：njhxzhangjihong@126.com
+ * Date：6/17 2016
+*/
+//打赏
+(function(){
+	$("#rewardTip").click(function(){
+		$("#dsWrapper").toggle("slow");
+	});
+})();
