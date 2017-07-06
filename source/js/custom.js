b2b398eef1a14d4b85296d505bbb7d74a8f52b57
jihong.zjh <jihong.zjh@alibaba-inc.com>
2017-07-06 10:07:01 +0800

    add new pages

--- source/js/custom.js
+++ source/js/custom.js
@@ -9,3 +9,23 @@
 		$("#dsWrapper").toggle("slow");
 	});
 })();
+
+// 域名切换
+(function(){
+    var html = '<div style="width:600px;text-align:center;position:absolute;top:50%;left:50%;margin-left:-300px;margin-top:-300px;border:1px solid darkblue;border-radius:6px;padding:20px"><div>从2017/7/6日起，本站域名切换为<a href="http://zhangjh.me"><span style="color:red;font-weight:bold;font-size:1.2em;font-style:oblique;">zhangjh.me</span></a></div>' + 
+               '<div><span id="timer">3</span>秒后自动跳转</div></div>';
+
+    var location = window.location.host;
+    var pathname = window.location.pathname;
+	if(location.indexOf("5941740") !== -1){
+        $("body").html(html);
+        var cnt = 2;
+        var timer = setInterval(function(){
+           $("#timer").text(cnt--);
+		   if(cnt === 0){
+              clearInterval(timer);
+              window.location.href = "http://zhangjh.me" + pathname;
+           }
+        },1000);
+    }
+})();
