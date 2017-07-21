bab8c8b7ad2ece88c6d6052234ed4f27c4355482
jihong.zjh <jihong.zjh@alibaba-inc.com>
2017-07-21 18:05:32 +0800

    add new pages

--- source/js/custom.js
+++ source/js/custom.js
@@ -11,6 +11,7 @@
 })();
 
 // 域名切换
+/*
 (function(){
     var html = '<div style="width:600px;text-align:center;position:absolute;top:50%;left:50%;margin-left:-300px;margin-top:-300px;border:1px solid darkblue;border-radius:6px;padding:20px"><div>本站域名后续将切换为<a href="http://zhangjh.me"><span style="color:red;font-weight:bold;font-size:1.2em;font-style:oblique;">zhangjh.me(备案中暂时不能访问)</span></a></div>' + 
                '<div><span id="timer">3</span>秒后自动跳转</div></div>';
@@ -18,7 +19,7 @@
     var location = window.location.host;
     var pathname = window.location.pathname;
 	if(location.indexOf("5941740") !== -1){
-        /*$("body").html(html);
+        $("body").html(html);
         var cnt = 2;
         var timer = setInterval(function(){
            $("#timer").text(cnt--);
@@ -26,10 +27,11 @@
               clearInterval(timer);
               window.location.href = "http://5941740.cn" + pathname;
            }
-        },1000);*/
+        },1000);
 		alert("本站域名后续将切换为zhangjh.me(备案中暂时不能访问)");
     }
     if(location.indexOf("zhangjh") !== -1){
       window.location.href = "http://5941740.cn" + pathname;
     }
 })();
+*/
