ceb1f269ff1e441cc7af0487b41507cd1d51bd2f
zhangjh <njhxzhangjihong@126.com>
2018-04-04 13:50:39 +0800

    增加评论回复消息通知功能

--- source/js/custom.js
+++ source/js/custom.js
@@ -53,3 +53,89 @@
 // 		window.open("http://zfb.zhangjh.me");
 // 	}
 // })();
+
+function getUrl(decodeStr) {
+    return decodeStr.replace(new RegExp("_","g"),"/");
+}
+
+// 消息列表点击事件注册
+function msgClickReg() {
+    $("#modal-body > .item > a").click(function () {
+        let id = $(this).attr("data-id");
+        let url = $(this).attr("data-url");
+        $.ajax({
+            url: "https://favlink.cn/comment/update",
+            data: {
+                condition: {
+                    _id: id
+                },
+                data: {
+                    readed: true
+                }
+            }
+        }).done(function (ret) {
+            if(ret.status){
+                // 定位到评论
+                window.location.href = url + "#comment-wrap";
+            }
+        });
+    });
+}
+
+(function msgList() {
+    let url = decodeURI(window.location.pathname.replace(new RegExp('\\/|\\.', 'g'),"_"));
+    let storage = window.localStorage;
+    let userInfo = storage.getItem("userInfo");
+    if(userInfo){
+        let userInfoObj = JSON.parse(userInfo);
+        let name = userInfoObj.name;
+        let queryUrl = "https://favlink.cn/comment/find";
+        let replyComments = [];
+        $.ajax({
+            url: queryUrl,
+            data: {
+                name: name,
+                isDeleted: false
+            }
+        }).done(function (ret) {
+            if(ret.status){
+                // 找出用户回复过的评论，作为replyId查询被回复的评论
+                let ids = [];
+                for(let item of ret.data){
+                    $.ajax({
+                        url: queryUrl,
+                        data: {
+                            replyId: item["_id"],
+                            readed: false,
+                            isDeleted: false
+                        },
+                        async: false
+                    }).done(function (result) {
+                        if(result.status){
+                            replyComments = replyComments.concat(result.data);
+                        }
+                    });
+                }
+
+                let total = replyComments.length;
+                if(total > 0){
+                    $("#msg-tip span").text(total);
+                    $("#msg-tip").show();
+                    let html = "";
+                    for(let item of replyComments){
+                        let pageUrl = item.url;
+                        let name = item.name;
+                        html += '<div class="item"><a data-id="' + item._id + '" data-url="' + getUrl(pageUrl) + '">' + name + '回复你</a></div>';
+                    }
+                    $("#modal-body").html(html);
+                    msgClickReg();
+                }
+            }
+        });
+
+        $("#msg-tip").click(function () {
+            $("#msg-modal").show();
+            $(this).hide();
+        });
+    }
+})();
