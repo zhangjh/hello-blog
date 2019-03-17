afd500d78befb76421d73897083bd6efdbb37d2c
zhangjh <njhxzhangjh@gmail.com>
2019-03-17 17:14:56 +0800

    fix

--- source/js/custom.js
+++ source/js/custom.js
@@ -144,8 +144,10 @@ function msgClickReg() {
     !function (e, t, a) {
         function r() {
             for (var e = 0; e < s.length; e++) {
-				s[e].alpha <= 0 ? (t.body.removeChild(s[e].el), s.splice(e, 1)) : (s[e].y--, s[e].scale += .004, s[e].alpha -= .013;
-				s[e].el.style.cssText = "left:" + s[e].x + "px;top:" + s[e].y + "px;opacity:" + s[e].alpha + ";transform:scale(" + s[e].scale + "," + s[e].scale + ") rotate(45deg);background:" + s[e].color + ";z-index:99999");
+				s[e].alpha <= 0 ? 
+						(t.body.removeChild(s[e].el), s.splice(e, 1)) : 
+						(s[e].y--, s[e].scale += .004, s[e].alpha -= .013,
+						s[e].el.style.cssText = "left:" + s[e].x + "px;top:" + s[e].y + "px;opacity:" + s[e].alpha + ";transform:scale(" + s[e].scale + "," + s[e].scale + ") rotate(45deg);background:" + s[e].color + ";z-index:99999");
             	requestAnimationFrame(r);
 			}
         }
