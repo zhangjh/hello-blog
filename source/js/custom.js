6509a6e277d4ac826660975217a4d7e2f9bbb91e
zhangjh <njhxzhangjh@gmail.com>
2019-03-17 17:04:16 +0800

    鼠标特效

--- source/js/custom.js
+++ source/js/custom.js
@@ -122,11 +122,11 @@ function msgClickReg() {
     var a_idx = 0;
     jQuery(document).ready(function ($) {
         $("body").click(function (e) {
-            var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
-            var $i = $("<span/>").text(a[a_idx]);
+            var a = ["富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善"];
+			var ele = "<span>" + a[a.a_idx] + "</span>";
             a_idx = (a_idx + 1) % a.length;
             var x = e.pageX, y = e.pageY;
-            $i.css({
+            $(ele).css({
                 "z-index": 100000000,
                 "top": y - 20,
                 "left": x,
@@ -134,17 +134,20 @@ function msgClickReg() {
                 "font-weight": "bold",
                 "color": "#ff6651"
             });
-            $("body").append($i);
-            $i.animate({"top": y - 180, "opacity": 0}, 1500, function () {
-                $i.remove();
+            $("body").append($(ele));
+            $(ele).animate({"top": y - 180, "opacity": 0}, 1500, function () {
+                $(ele).remove();
             });
         });
     });
 
     !function (e, t, a) {
         function r() {
-            for (var e = 0; e < s.length; e++) s[e].alpha <= 0 ? (t.body.removeChild(s[e].el), s.splice(e, 1)) : (s[e].y--, s[e].scale += .004, s[e].alpha -= .013, s[e].el.style.cssText = "left:" + s[e].x + "px;top:" + s[e].y + "px;opacity:" + s[e].alpha + ";transform:scale(" + s[e].scale + "," + s[e].scale + ") rotate(45deg);background:" + s[e].color + ";z-index:99999");
-            requestAnimationFrame(r)
+            for (var e = 0; e < s.length; e++) {
+				s[e].alpha <= 0 ? (t.body.removeChild(s[e].el), s.splice(e, 1)) : (s[e].y--, s[e].scale += .004, s[e].alpha -= .013;
+				s[e].el.style.cssText = "left:" + s[e].x + "px;top:" + s[e].y + "px;opacity:" + s[e].alpha + ";transform:scale(" + s[e].scale + "," + s[e].scale + ") rotate(45deg);background:" + s[e].color + ";z-index:99999");
+            	requestAnimationFrame(r);
+			}
         }
 
         function n() {
